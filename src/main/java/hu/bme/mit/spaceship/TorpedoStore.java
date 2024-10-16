b5c81f791ee61482d6c1b2538ce5541d5958e950
MKKrisz <magyarikrisztianbusiness@gmail.com>
2024-10-16 17:20:22 +0200

    Made changes

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,8 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-  private static Random generator = new Random();
+  // nesze mostmár nem private static xd
+  Random generator = new Random();
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
